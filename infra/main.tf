15028839d42c1027f09d0e05e2b2d9596b1a9b21
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-02 20:54:16 +0200

    Remove provider

--- infra/main.tf
+++ infra/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.2.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 module "vpc" {
   source       = "modules/vpc"
   project_name = var.project_name
